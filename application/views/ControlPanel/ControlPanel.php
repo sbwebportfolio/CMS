d1abc21f3e83bcf2712615facaff9076d6687ebb
Luca Scalzotto <luca@scorpiac.com>
2017-02-04 13:01:14 +0100

    Remove leftover script

--- application/views/ControlPanel/ControlPanel.php
+++ application/views/ControlPanel/ControlPanel.php
@@ -14,7 +14,6 @@
 		<!-- Scripts -->
 		<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 		<script type="text/javascript" src="/assets/js/ControlPanel/controlpanel.js"></script>
-		<script type="text/javascript" src="//cdn.ckeditor.com/4.6.1/standard/ckeditor.js"></script>
 	</head>
 	<body>
 		<div id="container">
